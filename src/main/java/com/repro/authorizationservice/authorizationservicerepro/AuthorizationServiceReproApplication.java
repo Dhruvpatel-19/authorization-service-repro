cdb379ec02a96a85092dd8971a3d20e03153eae7
Dhruv <dhruvpatel19201@gmail.com>
2022-09-12 17:29:08 +0530

    authorization-service-repro initial stage

---
+++ src/main/java/com/repro/authorizationservice/authorizationservicerepro/AuthorizationServiceReproApplication.java
@@ -0,0 +1,13 @@
+package com.repro.authorizationservice.authorizationservicerepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServiceReproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServiceReproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/repro/authorizationservice/authorizationservicerepro/AuthorizationServiceReproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repro.authorizationservice.authorizationservicerepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServiceReproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
